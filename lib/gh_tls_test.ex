a36537e37161fe735b81fe33cf9df5502a0d6936
Chris Dosé <chris@xn--dos-dma.com>
2024-02-11 18:31:08 -0800

    simplify

--- lib/gh_tls_test.ex
+++ lib/gh_tls_test.ex
@@ -1,18 +1,2 @@
 defmodule GhTlsTest do
-  @moduledoc """
-  Documentation for `GhTlsTest`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> GhTlsTest.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- mix.exs
+++ mix.exs
@@ -5,40 +5,16 @@ defmodule GhTlsTest.MixProject do
     [
       app: :gh_tls_test,
       version: "0.1.0",
-      elixir: "~> 1.14",
-      elixirc_paths: elixirc_paths(Mix.env()),
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
-      aliases: aliases(),
-      deps: deps(),
-      npm_deps: npm_deps(),
-      npm_deps: npm_deps(),
-      test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [
-        coveralls: :test,
-        "coveralls.html": :test,
-        "coveralls.lcov": :test
-      ],
-      dialyzer: [
-        plt_add_apps: [:mix, :ex_unit]
-      ]
+      deps: deps()
     ]
   end
-
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(:dev), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
 
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      mod: {GhTlsTest.Application, []},
-      extra_applications: [:logger, :xmerl]
-    ]
-  end
-
-  def npm_deps do
-    [
-      {:topbar, "2.0.2"}
+      extra_applications: [:logger]
     ]
   end
 
@@ -47,14 +23,7 @@ defmodule GhTlsTest.MixProject do
     [
       {:castore, ">= 0.0.0"},
       {:req, "~> 0.4"},
-      {:npm_deps, "~> 0.3", runtime: false},
       {:sweet_xml, "~> 0.7"}
-    ]
-  end
-
-  defp aliases do
-    [
-      "deps.get": ["deps.get", "npm_deps.get"]
     ]
   end
 end
